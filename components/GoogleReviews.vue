52c9f9782ea5b1951d080a37b365a5cc5760b8ed
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-06-16 22:43:27 +0200

    Fix spacing in GoogleReviews component: adjust margin for star rating and button

--- components/GoogleReviews.vue
+++ components/GoogleReviews.vue
@@ -2,7 +2,7 @@
   <h3 class="text-2xl font-semibold mt-12">
     Bewertungen
   </h3>
-  <div class="text-yellow-500 text-2xl mb-4">
+  <div class="text-yellow-500 text-2xl mb-1">
     <UIcon name="i-heroicons-star-solid" />
     <UIcon name="i-heroicons-star-solid" />
     <UIcon name="i-heroicons-star-solid" />
@@ -12,6 +12,10 @@
       class="opacity-30"
     />
   </div>
+  <UButton
+    class="mb-6"
+    label="Auf Google bewerten"
+  />
   <div class="italic text-gray-500 text-sm mb-4">
     "AutoFAQ ist ein großartiges Tool, um meine Kundenanfragen zu verwalten. Es spart mir viel Zeit und hilft mir, professionell zu wirken." - Markus Kottländer
   </div>
