fbbb6f7e177f3abcb71a679fb1f534a7cbdc4154
mktcode <kontakt@markus-kottlaender.de>
2025-06-16 17:03:56 +0200

    Remove authentication middleware: delete unused route protection logic

--- middleware/auth.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  const { loggedIn } = useUserSession()
-  const publicPaths = ['/login', '/', '/datenschutz', '/impressum', '/register']
-
-  if (!loggedIn.value && !publicPaths.includes(to.path)) {
-    return navigateTo('/login')
-  }
-})
