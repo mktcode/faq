fa49558750779e0e48466af336d889801b7f0d04
mktcode <kontakt@markus-kottlaender.de>
2025-06-17 14:09:55 +0200

    Refactor Gallery component: enhance image layout and styling for better responsiveness

--- components/Ask/Gallery.vue
+++ components/Ask/Gallery.vue
@@ -1,36 +1,22 @@
 <template>
   <div class="flex flex-col gap-2">
-    <div>
+    <div class="rounded-xl overflow-hidden w-full aspect-video flex items-center justify-center">
       <img
-        src="https://picsum.photos/800/400"
-        class="rounded-lg"
+        src="https://picsum.photos/800/800"
+        class="object-cover w-full hover:scale-110 transition-transform duration-500"
       >
     </div>
     <div class="grid grid-cols-2 lg:grid-cols-3 gap-2">
-      <img
-        src="https://picsum.photos/seed/1/300/300"
-        class="rounded-lg"
+      <div
+        v-for="index in 6"
+        :key="index"
+        class="rounded-xl overflow-hidden w-full aspect-square flex items-center justify-center"
       >
-      <img
-        src="https://picsum.photos/seed/2/300/300"
-        class="rounded-lg"
-      >
-      <img
-        src="https://picsum.photos/seed/3/300/300"
-        class="rounded-lg"
-      >
-      <img
-        src="https://picsum.photos/seed/4/300/300"
-        class="rounded-lg"
-      >
-      <img
-        src="https://picsum.photos/seed/5/300/300"
-        class="rounded-lg"
-      >
-      <img
-        src="https://picsum.photos/seed/6/300/300"
-        class="rounded-lg"
-      >
+        <img
+          :src="`https://picsum.photos/seed/${index + Math.random()}/400/400`"
+          class="object-cover w-full hover:scale-110 transition-transform duration-500"
+        >
+      </div>
     </div>
   </div>
 </template>
