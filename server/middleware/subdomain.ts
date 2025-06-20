65b9e4c2b09836ab8e7a56437711cf3005b0e060
mktcode <kontakt@markus-kottlaender.de>
2025-06-20 17:42:08 +0200

    Add logging for host value in subdomain middleware

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler(({ req, context }) => {
   const { public: { host } } = useRuntimeConfig()
 
+  console.log('Host:', req.headers.host, host)
   const hasSubdomain = !(req.headers.host || host).startsWith(host)
+  console.log('Has subdomain:', hasSubdomain)
 
   if (hasSubdomain) {
     const subdomain = (req.headers.host || host).split('.')[0]
