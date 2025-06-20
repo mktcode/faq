8c8556a8a6e0cf7e8da07744fe9ee030a4cf0c03
mktcode <kontakt@markus-kottlaender.de>
2025-06-20 18:10:15 +0200

    Remove logging statements for host and subdomain in subdomain middleware

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,9 +1,7 @@
 export default defineEventHandler(({ req, context }) => {
   const { public: { appHost } } = useRuntimeConfig()
 
-  console.log('Host:', req.headers.host, appHost)
   const hasSubdomain = !(req.headers.host || appHost).startsWith(appHost)
-  console.log('Has subdomain:', hasSubdomain)
 
   if (hasSubdomain) {
     const subdomain = (req.headers.host || appHost).split('.')[0]
