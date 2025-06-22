033df403e56234744f114dbf9acc4b6a8b780dd9
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-06-22 17:05:46 +0200

    Add Google reviews management to SettingsModal and create API endpoint

--- components/SettingsModal/Google.vue
+++ components/SettingsModal/Google.vue
@@ -3,7 +3,11 @@ const toast = useToast()
 
 const emit = defineEmits(['update'])
 
+const { me } = await useMe()
+
 const { data: currentSettings } = await useFetch(`/api/user/settings`)
+
+const { data: reviews } = await useFetch('/api/user/google/reviews')
 
 const form = ref({
   showGoogleReviews: currentSettings.value?.showGoogleReviews || false,
@@ -35,6 +39,31 @@ async function saveSettings() {
         class="mt-3"
       />
     </UFormField>
+    <div v-if="form.showGoogleReviews">
+      <div v-if="!me?.googleId" class="text-red-600">
+        Du hast noch kein Google-Konto verbunden. Bitte verbinde dein Konto, um
+        Google Bewertungen anzuzeigen.
+
+        <UButton
+          label="Google verbinden"
+          variant="solid"
+          color="primary"
+          class="mt-2"
+        />
+      </div>
+      <div v-else>
+        <p class="text-gray-700">
+          Du kannst deine Google Bewertungen hier verwalten. Aktuell sind
+          folgende Bewertungen verfügbar:
+        </p>
+        <ul class="list-disc pl-5 mt-2">
+          <li v-for="(review, index) in reviews" :key="index">
+            {{ review }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
     <UButton
       variant="solid"
       color="primary"
