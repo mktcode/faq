9c33da4aa30a758be3f02dab6869aad0b9594e46
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-06-28 14:02:04 +0200

    Refactor UpgradeModal: streamline content and remove redundant sections for clarity

--- components/UpgradeModal/index.vue
+++ components/UpgradeModal/index.vue
@@ -22,46 +22,28 @@ const showModal = useState('showUpgradeModal', () => false)
           @click="showModal = false"
         />
       </div>
-      <p class="text-gray-500 mb-8">
+      <p class="text-gray-500 mb-4">
         Mit einem individuellen Premium-Design steht Ihr Gewerbeprofil einer richtigen Website in nichts nach.
-        Wir besprechen Ihre Farben, Schriftarten und zusätzliche Inhalte und gestalten  ein Webdesign, das zu Ihrem Unternehmen passt.
+        Wir besprechen Ihre Farben, Schriftarten und zusätzliche Inhalte und gestalten ein Design, das auf Ihr Unternehmen zugeschnitten ist.
       </p>
-      <div class="flex flex-col gap-2">
-        <div class="border border-gray-300 bg-green-50/10 rounded-lg p-4 w-full flex flex-col">
-          <h3 class="text-lg font-semibold mb-4">
-            Domain und E-Mail
-          </h3>
-          <p class="text-gray-500">
-            Sie brauchen erstmal nicht mehr als Ihr Gewerbeprofil, aber eine eigene Domain und E-Mail-Adresse muss schon sein?
-          </p>
-          <div class="pt-4 mt-auto flex items-center justify-between">
-            <div class="text-gray-700 text-xl font-semibold">
-              1 € pro Monat
-            </div>
-            <UButton
-              label="Domain sichern"
-            />
-          </div>
-        </div>
-        <div class="border border-gray-300 bg-green-50/10 rounded-lg p-4">
-          <h3 class="text-lg font-semibold mb-4">
-            Haben Sie noch einen Wunsch?
-          </h3>
-          <p class="text-gray-500">
-            Sie haben spezielle Wünsche oder benötigen eine individuelle Lösung, die hier nicht aufgeführt ist?
-            Kein Problem, wir sind flexibel und finden gemeinsam eine Lösung.
-          </p>
-          <UTextarea
-            placeholder="Beschreiben Sie hier Ihre Wünsche..."
-            class="w-full mt-4"
-          />
-          <div class="pt-4 mt-auto flex items-center justify-between">
-            <UButton
-              label="Anfrage senden"
-              class="ml-auto"
-            />
-          </div>
-        </div>
+      <p class="text-gray-500 mb-8">
+        Senden Sie uns eine unverbindliche Anfrage und Sie erhalten innerhalb von 7 Tagen einen Designvorschlag von uns.
+      </p>
+      <h3 class="text-lg font-semibold mb-4">
+        Haben Sie schon eine konkrete Vorstellung?
+      </h3>
+      <p class="text-gray-500">
+        Füllen Sie Ihr Gewerbeprofil so gut es geht aus. Darüber hinaus können Sie uns hier gerne Ihre Wünsche und Ideen mitteilen.
+      </p>
+      <UTextarea
+        placeholder="Geben Sie hier Ihre Wünsche und Ideen ein..."
+        class="w-full mt-4"
+      />
+      <div class="pt-4 mt-auto flex items-center justify-between">
+        <UButton
+          label="Anfrage senden"
+          class="ml-auto"
+        />
       </div>
     </template>
   </UModal>
