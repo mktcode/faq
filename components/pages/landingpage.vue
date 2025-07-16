ef6b06f2fdd512090558925212e86e72ceaef398
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-16 16:12:26 +0200

    fix: update landing page content and layout for improved user engagement

--- components/pages/landingpage.vue
+++ components/pages/landingpage.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import backgroundUrl from '~/assets/img/lp-bg.png'
-
 const { public: { appHost } } = useRuntimeConfig()
 const { clear } = useUserSession()
 const { me, refreshMe } = await useMe()
@@ -13,17 +11,16 @@ async function signOut() {
 
 <template>
   <div
-    class="font-poppins flex flex-col items-center justify-center max-w-3xl mx-auto py-24 md:py-32 px-4 bg-contain bg-top bg-no-repeat"
-    :style="{ backgroundImage: `url(${backgroundUrl})` }"
+    class="font-poppins flex flex-col items-center justify-center max-w-3xl mx-auto py-24 md:py-32 px-4"
   >
     <div
       class="text-center flex flex-col items-center max-w-2xl mx-auto"
     >
       <h2 class="text-2xl">
-        Gerade selbstständig gemacht?
+        Wagen Sie gerade den Schritt in die Selbstständigkeit?
       </h2>
       <h1 class="text-5xl font-bold mt-2">
-        Brauchen Sie eine Website?
+        Werden Sie sichtbar!
       </h1>
       <div class="mt-8 flex gap-2">
         <template
@@ -44,7 +41,7 @@ async function signOut() {
           <UButton
             to="/register"
           >
-            Nein, erstmal nicht.
+            Registrieren
           </UButton>
           <UButton
             to="/login"
@@ -58,48 +55,45 @@ async function signOut() {
 
     <section class="mt-24 shadow-sky-50 flex flex-col">
       <p class="mt-4 text-gray-500 text-xl">
-        Bevor Sie zu viel (oder zu wenig) Geld für eine Website ausgeben,
-        probieren Sie unser kostenloses Gewerbeprofil aus. Damit sind Sie sofort online und konzentrieren sich auf Ihre Inhalte und die Kommunikation mit Ihren Kunden.
-      </p>
-      <p class="mt-4 text-gray-500 text-xl">
-        Eine individuelle, ästhetisch und inhaltlich professionell gestaltete Website ist dann der nächste Schritt und mit Ihrem Gewerbeprofil sind Sie bereits bestens vorbereitet.
-        Schauen Sie sich am besten ein paar Beispiele an:
+        Das geht auch ohne großes Budget. Sogar kostenlos kommen Sie schon sehr weit.
+        Wir wollen verhindern, dass Ihr Unternehmergeist durch hohe Kosten und überdimensionierte Lösungen ausgebremst wird.
       </p>
     </section>
 
-    <UCarousel
-      v-slot="{ item }"
-      loop
-      :auto-scroll="{
-        stopOnFocusIn: false,
-        stopOnInteraction: false,
-        stopOnMouseEnter: false,
-        speed: 1,
-      }"
-      class="w-full mask-l-from-90% mask-r-from-90% max-w-3xl my-12"
-      :items="[
-        '',
-        '',
-        'https://nbg1.your-objectstorage.com/mktcms/1/icon.webp',
-        '',
-        '',
-        '',
-      ]"
-      :ui="{ item: 'basis-1/4' }"
-    >
-      <a
-        :href="`https://markuskottlaender.${appHost}`"
-        target="_blank"
-        class="block w-full aspect-square rounded-full"
-        :class="{ 'bg-sky-50': !item }"
-      >
-        <NuxtImg
-          v-if="item"
-          :src="item"
-          class="object-cover w-full h-full rounded-lg bg-white"
-        />
-      </a>
-    </UCarousel>
+    <ul class="mt-10 flex flex-col gap-4 text-gray-500 text-lg">
+      <li class="flex items-start">
+        <span class="text-sky-500 text-4xl mr-2">✔</span>
+        <span>
+          <strong class="text-gray-900">Ist Ihr Angebot in einem Satz erklärbar? Wer (genau!) ist Ihre Zielgruppe?</strong><br>
+          Wenn Sie nicht klar sagen können, was Sie wem bringen – wird’s auch kein anderer verstehen.
+          Kostenlose KI-Tools wie <a class="text-sky-500" href="https://chat.openai.com/" target="_blank">ChatGPT</a> oder <a class="text-sky-500" href="https://gemini.google.com" target="_blank">Google Gemini</a> können Ihnen helfen, Klarheit zu gewinnen.
+        </span>
+      </li>
+      <li class="flex items-start">
+        <span class="text-sky-500 text-4xl mr-2">✔</span>
+        <span>
+          <strong class="text-gray-900">Google Unternehmensprofil eingerichtet?</strong><br>
+          Das ist ebenfalls kostenlos und wesentlich wichtiger als eine Website, die ansonsten ohnehin niemand findet.
+          Legen Sie sich ein <a class="text-sky-500" href="https://accounts.google.com/signup" target="_blank">Gmail-Konto</a> an und schauen Sie sich eine der zahlreichen <a class="text-sky-500" href="https://www.youtube.com/results?search_query=google+unternehmensprofil+erstellen" target="_blank">Anleitungen auf YouTube</a> an.
+        </span>
+      </li>
+      <li class="flex items-start">
+        <span class="text-sky-500 text-4xl mr-2">✔</span>
+        <span><strong class="text-gray-900">Erste Kundenmeinungen oder Referenzen?</strong> <br>
+        Bewertungen zählen mehr als ein schickes Design.</span>
+      </li>
+      <li class="flex items-start">
+        <span class="text-sky-500 text-4xl mr-2">✔</span>
+        <span><strong class="text-gray-900">Aktives Social Media Profil?</strong> <br>
+        Egal ob Instagram, LinkedIn oder TikTok – Hauptsache Sie zeigen sich und interagieren.</span>
+      </li>
+    </ul>
+
+    <p class="mt-10 text-gray-500 text-xl">
+      Haben Sie diese Punkte abgehakt, können Sie sich um eine Website kümmern,... wenn Sie dann überhaupt noch eine brauchen.
+      Bevor Sie zu viel (oder zu wenig) Geld für eine Website ausgeben,
+      probieren Sie unser kostenloses Gewerbeprofil aus. Damit sind Sie sofort online und konzentrieren sich auf Ihre Inhalte und die Kommunikation mit Ihren Kunden.
+    </p>
 
     <UButton
       to="/register/2"
