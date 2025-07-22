5a6e05cee0f6d5f08082c0c318b27b608de96a05
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-22 23:52:46 +0200

    feat: implement active state management in accordion component

--- components/TestAcc.vue
+++ components/TestAcc.vue
@@ -31,6 +31,7 @@ const items = shallowRef<AccordionItem[]>([
 ])
 
 const accordion = useTemplateRef<HTMLElement>('accordion')
+const active = ref<string | undefined>(undefined)
 
 useSortable(accordion, items, {
   animation: 150
@@ -39,6 +40,7 @@ useSortable(accordion, items, {
 
 <template>
   <UAccordion
+    v-model="active"
     ref="accordion"
     :items="items"
     :unmount-on-hide="false"
@@ -46,6 +48,24 @@ useSortable(accordion, items, {
       header: 'px-4 hover:bg-gray-50',
     }"
   >
+    <template #trailing="{ open }">
+      <div class="ml-auto flex gap-2 items-center">
+        <Transition name="fade">
+          <UIcon
+            v-if="active === undefined"
+            name="i-heroicons-arrows-up-down"
+            class="text-gray-500 size-5"
+          />
+        </Transition>
+        <UIcon
+          name="i-heroicons-chevron-down"
+          class="text-gray-500 size-5"
+          :class="{
+            'rotate-180': open,
+          }"
+        />
+      </div>
+    </template>
     <template #gallery>
       <ContentModalGallery />
     </template>
