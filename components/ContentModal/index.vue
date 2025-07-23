1af3291afb3e2488f56d50ddb3d24437178146e6
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-23 16:12:37 +0200

    fix: update alert title and message for clarity and improved user guidance

--- components/ContentModal/index.vue
+++ components/ContentModal/index.vue
@@ -17,20 +17,26 @@ const showModal = useState('showContentModal', () => false)
           name="i-lucide-letter-text"
           class="inline-block size-6 opacity-50"
         />
-        Inhalt und Funktionen
+        Inhalt und Funktion
       </h3>
     </template>
     <template #body>
       <DismissableAlert
-        title="Wichtige Information"
+        title="Sichtbarkeit und Reihenfolge"
         icon="i-heroicons-light-bulb"
         storageKey="content-info-dismissed"
         class="rounded-none"
       >
-        Wir gehen davon aus, dass Sie am Anfang Ihrer Selbstständigkeit stehen.
-        Viele Anforderungen gelten erst aber einer bestimmten Mitarbeiterzahl.
-        Wenn Sie bereits mehr als 10 Mitarbeiter haben, ist ein Gewerbeprofil nicht das Richtige für Sie.
-        <a href="https://markus-kottlaender.de" target="_blank" class="underline">Kontaktieren Sie uns</a> für eine individuelle Beratung.
+        Ihr Gewerbeprofil besteht aus den unten aufgeführten Inhalten. Klicken und Ziehen sie einen Eintrag, um die Reihenfolge zu ändern
+        <UIcon
+          name="i-heroicons-arrows-up-down"
+          class="inline-block size-4 text-white align-top"
+        />
+        und blenden Sie Bereiche aus, die Sie nicht nutzen bzw. (noch) nicht anzeigen möchten.
+        <UIcon
+          name="i-heroicons-eye"
+          class="inline-block size-4 text-white align-top"
+        />
       </DismissableAlert>
       <ContentModalAccordion />
     </template>
