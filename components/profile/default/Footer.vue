73d487453e298e0f7ff0192eeaf6908b771254e5
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-23 16:48:28 +0200

    feat: add Footer component to Main.vue and refactor footer links into separate component

---
+++ components/profile/default/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex items-center justify-center gap-2 mt-12 text-sm">
+    <ULink
+      to="/impressum"
+      class="text-gray-400"
+    >
+      Impressum
+    </ULink>
+    <ULink
+      to="/datenschutz"
+      class="text-gray-400"
+    >
+      Datenschutz
+    </ULink>
+  </div>
+</template>
--- components/profile/default/Main.vue
+++ components/profile/default/Main.vue
@@ -109,20 +109,7 @@ const designRounded = useState('designRounded', () => currentSettings.value?.rou
         </UAccordion>
       </div>
 
-      <div class="w-full flex items-center justify-center gap-2 mt-12 text-sm">
-        <ULink
-          to="/impressum"
-          class="text-gray-400"
-        >
-          Impressum
-        </ULink>
-        <ULink
-          to="/datenschutz"
-          class="text-gray-400"
-        >
-          Datenschutz
-        </ULink>
-      </div>
+      <ProfileDefaultFooter />
     </div>
 
     <div class="w-full mt-6 py-24 border-t border-gray-200">
