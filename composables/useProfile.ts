13138fd35a90da6805f69e7f2c9403d80043e3c1
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-31 22:12:23 +0200

    fix: settings state management and fetch logic in useProfile

--- composables/useProfile.ts
+++ composables/useProfile.ts
@@ -1,3 +1,5 @@
+import type { SettingsForm } from "~/types/db"
+
 export const useProfile = async () => {
   const { ssrContext } = useNuxtApp()
   const path = useRoute().path
@@ -7,6 +9,9 @@ export const useProfile = async () => {
   const isOwned = useState<boolean>('isOwned', () => false)
   const isPublic = useState<boolean>('isPublic', () => false)
   const design = useState<string>('design', () => 'default')
+  const settings = useState<SettingsForm | null>('settings', () => null)
+
+  let refreshSettings = async () => {}
 
   if (ssrContext?.event.context.profile) {
     username.value = ssrContext.event.context.profile.username
@@ -14,13 +19,17 @@ export const useProfile = async () => {
     isOwned.value = ssrContext.event.context.profile.isOwned
     isPublic.value = ssrContext.event.context.profile.isPublic
     design.value = ssrContext.event.context.profile.design
+    
+    const { data: currentSettings, refresh } = await useFetch(`/api/settings`, {
+      query: {
+        username,
+      },
+    })
+
+    settings.value = currentSettings.value
+    refreshSettings = refresh
   }
 
-  const { data: settings, refresh: refreshSettings } = await useFetch(`/api/settings`, {
-    query: {
-      username,
-    },
-  })
 
   const showLegalDataWarning = computed(() => isOwned && (!settings.value?.company?.name || !settings.value?.company?.city || !settings.value?.company?.street || !settings.value?.company?.zip || !settings.value?.company?.email))
 
