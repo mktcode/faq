7b11463a8336a3dfe2373535a9667b9a6286af51
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-07-31 18:15:48 +0200

    refactor: replace useFetch with useProfile for settings management

--- components/profile/default/Main.vue
+++ components/profile/default/Main.vue
@@ -9,29 +9,25 @@ const { username } = defineProps<{
 
 const { me } = await useMe()
 
-const { data: currentSettings, refresh: refreshSettings } = await useFetch(`/api/settings`, {
-  query: {
-    username,
-  },
-})
+const { settings, refreshSettings } = await useProfile()
 
 useHead({
-  title: currentSettings.value?.title || 'Meine Website',
+  title: settings.value?.title || 'Meine Website',
   meta: [
     {
       name: 'description',
-      content: currentSettings.value?.description || 'Mein Unternehmen auf einem Blick',
+      content: settings.value?.description || 'Mein Unternehmen auf einem Blick',
     },
   ],
 })
 
 const appConfig = useAppConfig()
-const font = computed(() => currentSettings.value?.font || 'roboto')
-appConfig.ui.colors.primary = currentSettings.value?.color || 'sky'
-appConfig.ui.button.defaultVariants.rounded = currentSettings.value?.rounded || 'md'
-appConfig.ui.input.defaultVariants.rounded = currentSettings.value?.rounded || 'md'
-appConfig.ui.select.defaultVariants.rounded = currentSettings.value?.rounded || 'md'
-appConfig.ui.textarea.defaultVariants.rounded = currentSettings.value?.rounded || 'md'
+const font = computed(() => settings.value?.font || 'roboto')
+appConfig.ui.colors.primary = settings.value?.color || 'sky'
+appConfig.ui.button.defaultVariants.rounded = settings.value?.rounded || 'md'
+appConfig.ui.input.defaultVariants.rounded = settings.value?.rounded || 'md'
+appConfig.ui.select.defaultVariants.rounded = settings.value?.rounded || 'md'
+appConfig.ui.textarea.defaultVariants.rounded = settings.value?.rounded || 'md'
 </script>
 
 <template>
@@ -44,17 +40,17 @@ appConfig.ui.textarea.defaultVariants.rounded = currentSettings.value?.rounded |
     />
     <ProfileDefaultHeader
       :username="username"
-      :current-settings="currentSettings"
+      :current-settings="settings"
     />
     <div
-      v-if="currentSettings"
+      v-if="settings"
       class="flex flex-col items-center justify-center gap-2 max-w-lg mx-auto py-12 px-6"
     >
       <ProfileDefaultComponentViewer
         v-for="index in [0, 1, 2, 3, 4]"
         :key="index"
         :username="username"
-        :settings="currentSettings"
+        :settings="settings"
         :slot-index="index"
       />
 
