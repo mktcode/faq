f05aff06bdc4fd6b08763a8460ea8c0e602bd866
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-08-09 21:47:30 +0200

    feat: refactor checkout session handling and add utility functions for Stripe customer management

--- server/api/user/createCheckoutSession.ts
+++ server/api/user/createCheckoutSession.ts
@@ -1,88 +1,32 @@
-import Stripe from 'stripe'
+import { createCheckoutSession, requireCompleteStripeCustomer, requireNoStripeCustomerId, requireNoSubscription, requireVerifiedEmail } from '~/server/utils/checkout'
+import { settingsFormSchema } from '~/types/db'
 
 export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event)
   const db = await getDatabaseConnection()
-  const { stripeApiSecretKey, stripePriceId, public: { appHost } } = useRuntimeConfig()
-  const stripe = new Stripe(stripeApiSecretKey)
-
-  let stripeCustomerId = null
 
   const userInDb = await db
     .selectFrom('users')
-    .select(['id', 'stripeCustomerId', 'userName', 'email', 'emailConfirmationToken'])
+    .select(['id', 'stripeCustomerId', 'userName', 'email', 'emailConfirmationToken', 'settings', 'lastPaidAt'])
     .where('id', '=', user.id)
     .executeTakeFirstOrThrow()
 
-  if (!userInDb.email || userInDb.emailConfirmationToken) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: 'Bad Request: User does not have a verified email address',
-    })
-  }
+  requireNoSubscription(userInDb.lastPaidAt)
+  requireNoStripeCustomerId(userInDb.stripeCustomerId)
 
-  stripeCustomerId = userInDb.stripeCustomerId
+  const userWithVerifiedEmail = requireVerifiedEmail(userInDb)
 
-  if (!stripeCustomerId) {
-    const customers = await stripe.customers.list({
-      email: userInDb.email,
-    })
+  const settings = settingsFormSchema.parse(
+    typeof userWithVerifiedEmail.settings === 'string' ? JSON.parse(userWithVerifiedEmail.settings) : userWithVerifiedEmail.settings
+  )
 
-    const existingCustomerByUserId = customers.data.find(c => c.metadata.solihostUserId === userInDb.id.toString())
-    const existingCustomerByOldest = customers.data.sort((a, b) => a.created - b.created)[0]
+  const stripeCustomerId = await requireCompleteStripeCustomer(
+    userWithVerifiedEmail.id,
+    userWithVerifiedEmail.email,
+    settings,
+  )
 
-    if (existingCustomerByUserId) {
-      stripeCustomerId = existingCustomerByUserId.id
-    }
-    else if (existingCustomerByOldest) {
-      stripeCustomerId = existingCustomerByOldest.id
-      await stripe.customers.update(stripeCustomerId, {
-        metadata: {
-          ...existingCustomerByOldest.metadata,
-          solihostUserId: userInDb.id.toString(),
-        },
-      })
-    }
-    else {
-      const stripeCustomer = await stripe.customers.create({
-        email: userInDb.email,
-        metadata: {
-          solihostUserId: userInDb.id.toString(),
-        },
-      })
+  const checkoutSession = await createCheckoutSession(stripeCustomerId, userWithVerifiedEmail.userName)
 
-      stripeCustomerId = stripeCustomer.id
-    }
-
-    await db
-      .updateTable('users')
-      .set({ stripeCustomerId })
-      .where('id', '=', userInDb.id)
-      .execute()
-  }
-
-  if (!stripeCustomerId) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: 'Bad Request: User does not have a Stripe customer ID and it could not be created on the fly.',
-    })
-  }
-
-  const session = await stripe.checkout.sessions.create({
-    customer: stripeCustomerId,
-    success_url: `https://${userInDb.userName}.${appHost}?subscriptionSuccess=1`,
-
-    line_items: [
-      {
-        price: stripePriceId,
-        quantity: 1,
-      },
-    ],
-    automatic_tax: {
-      enabled: true,
-    },
-    mode: 'subscription',
-  })
-
-  return { success: true, url: session.url }
+  return { success: true, url: checkoutSession.url }
 })
---
+++ server/utils/checkout.ts
@@ -0,0 +1,112 @@
+import Stripe from "stripe"
+import { SettingsForm } from "~/types/db"
+
+export type HasEmailFields<TUser> = TUser & {
+  email: string | null
+  emailConfirmationToken: string | null
+}
+
+export type UserWithVerifiedEmail<TUser> = TUser & {
+  email: string
+  emailConfirmationToken: null
+}
+
+export function requireVerifiedEmail<TUser>(
+  user: HasEmailFields<TUser>,
+): UserWithVerifiedEmail<TUser> {
+  if (user.email && user.emailConfirmationToken === null) {
+    return user as UserWithVerifiedEmail<TUser>
+  }
+
+  throw createError({
+    statusCode: 400,
+    statusMessage: 'Bad Request: User does not have a verified email address',
+  })
+}
+
+export async function requireCompleteStripeCustomer(
+  userId: number,
+  email: string,
+  settings: SettingsForm
+): Promise<string> {
+  const db = await getDatabaseConnection()
+  const { stripeApiSecretKey } = useRuntimeConfig()
+  const stripe = new Stripe(stripeApiSecretKey)
+
+  const address = {
+    city: settings.company.city,
+    country: 'DE',
+    postal_code: settings.company.zip,
+  }
+  const metadata = {
+    solihostUserId: userId.toString(),
+  }
+  const tax = {
+    validate_location: 'immediately' as const
+  }
+
+  const customers = await stripe.customers.list({ email })
+
+  const existingCustomerByUserId = customers.data.find(c => c.metadata.solihostUserId === userId.toString())
+  const existingCustomerByOldest = customers.data.sort((a, b) => a.created - b.created)[0]
+
+  const foundCustomer = existingCustomerByUserId || existingCustomerByOldest
+
+  let finalCustomerId = null
+
+  if (foundCustomer) {
+    await stripe.customers.update(foundCustomer.id, { address, metadata, tax })
+    finalCustomerId = foundCustomer.id
+  }
+  else {
+    const newStripeCustomer = await stripe.customers.create({ email, address, metadata, tax })
+    finalCustomerId = newStripeCustomer.id
+  }
+
+  await db
+    .updateTable('users')
+    .set({ stripeCustomerId: finalCustomerId })
+    .where('id', '=', userId)
+    .execute()
+
+  return finalCustomerId
+}
+
+export async function createCheckoutSession(customerId: string, userName: string) {
+  const { stripeApiSecretKey, stripePriceId, public: { appHost } } = useRuntimeConfig()
+  const stripe = new Stripe(stripeApiSecretKey)
+
+  return await stripe.checkout.sessions.create({
+    customer: customerId,
+    success_url: `https://${userName}.${appHost}?subscriptionSuccess=1`,
+
+    line_items: [
+      {
+        price: stripePriceId,
+        quantity: 1,
+      },
+    ],
+    automatic_tax: {
+      enabled: true,
+    },
+    mode: 'subscription',
+  })
+}
+
+export function requireNoSubscription(lastPaidAt: Date | null): void {
+  if (checkSubscriptionStatus(lastPaidAt)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request: User has an active subscription',
+    })
+  }
+}
+
+export function requireNoStripeCustomerId(stripeCustomerId: string | null): void {
+  if (stripeCustomerId) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request: User already has a Stripe customer ID',
+    })
+  }
+}
