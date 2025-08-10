b8a5ec8e9f6eb576e851919718631010f2e528a4
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-08-10 17:39:06 +0200

    feat: redirect logged in users from /register to their website

--- components/pages/register.vue
+++ components/pages/register.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const { user } = useUserSession()
+const { appHost } = useRuntimeConfig().public
+
+if (user.value) {
+  navigateTo(`https://${user.value.userName}.${appHost}`, { external: true })
+}
+</script>
+
 <template>
   <div class="flex flex-col justify-between gap-12 max-w-xl mx-auto p-6">
     <SolihostLogo class="max-w-[150px] mb-12" />
