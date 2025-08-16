04001b2581b3cb19ae3ed9bd6db109163eb58796
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-08-16 18:16:46 +0200

    updated solihost logo

--- components/SolihostLogo.vue
+++ components/SolihostLogo.vue
@@ -6,18 +6,14 @@ const { beta = false } = defineProps<{
 
 <template>
   <div class="@container flex flex-col gap-[0.2cqw] mx-auto w-full">
-    <div class="text-[22cqw] font-bold drop-shadow-lg leading-none text-center">
-      Solihost
-    </div>
-    <div class="relative h-[5cqw]">
-      <div class="absolute rounded-l-full bg-gradient-to-r from-sky-600 via-sky-400 via-90% to-transparent h-[5cqw] w-full rotate-1 -left-[1cqw] opacity-50" />
-      <div class="absolute rounded-l-full bg-gradient-to-r from-sky-600 via-sky-400 via-90% to-transparent h-[5cqw] w-full -rotate-3 left-[1cqw] opacity-80" />
-    </div>
-    <div
-      v-if="beta"
-      class="text-gray-400 text-[5cqw] font-light ml-auto pr-[5cqw]"
-    >
-      beta
+    <div class="flex items-center gap-3">
+      <div
+        class="w-10 h-10 text-white font-bold text-2xl rounded-[13px] bg-[radial-gradient(100%_100%_at_30%_20%,#58d0ff_0%,#0ea5e9_40%,#04669e_100%)] grid place-items-center shadow-[0_10px_18px_rgba(14,165,233,.35)]"
+        aria-hidden="true"
+      >
+        S
+      </div>
+      <span class="font-extrabold tracking-[.2px]">Solihost</span>
     </div>
   </div>
 </template>
