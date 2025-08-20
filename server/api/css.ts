91af68897475e26f95b8cac3ae7759f99ce46204
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-08-20 08:27:14 +0200

    fix: color live chat button

--- server/api/css.ts
+++ server/api/css.ts
@@ -25,18 +25,22 @@ export default defineEventHandler(async (event) => {
   const lightness = parseFloat(match[3])
 
   const css = `:root {
-    --color-website-50: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 50)}%);
-    --color-website-100: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 40)}%);
-    --color-website-200: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 30)}%);
-    --color-website-300: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 20)}%);
-    --color-website-400: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 10)}%);
-    --color-website-500: ${settings.design.color};
-    --color-website-600: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 10)}%);
-    --color-website-700: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 20)}%);
-    --color-website-800: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 30)}%);
-    --color-website-900: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 40)}%);
-    --color-website-950: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 50)}%);
-    --color-website: var(--color-website-500);
+  --color-website-50: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 50)}%);
+  --color-website-100: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 40)}%);
+  --color-website-200: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 30)}%);
+  --color-website-300: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 20)}%);
+  --color-website-400: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness + 10)}%);
+  --color-website-500: ${settings.design.color};
+  --color-website-600: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 10)}%);
+  --color-website-700: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 20)}%);
+  --color-website-800: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 30)}%);
+  --color-website-900: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 40)}%);
+  --color-website-950: hsl(${hue}, ${saturation}%, ${clampPercentage(lightness - 50)}%);
+  --color-website: var(--color-website-500);
+}
+
+.woot-widget-bubble {
+  background-color: var(--color-website) !important;
 }
 
 ${settings.css}`
