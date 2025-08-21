f78ca6bd1cfb1c9f5c2d67defe78c356a8041091
Markus Kottländer <kontakt@markus-kottlaender.de>
2025-08-21 23:50:05 +0200

    removed old code

--- composables/useMe.ts
+++ composables/useMe.ts
@@ -5,15 +5,8 @@ export default async function useMe() {
     transform: data => data.data,
   })
 
-  const isMe = computed(() => {
-    if (!me.value) return false
-    const username = Array.isArray(route.params.username) ? route.params.username[0] : route.params.username
-    return me.value.userName === username
-  })
-
   return {
     me,
     refreshMe: refresh,
-    isMe,
   }
 }
