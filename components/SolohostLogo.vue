d9e1ff944da50677e205b38d5c5118d680a2dd12
mktcode <kontakt@markus-kottlaender.de>
2025-08-29 12:14:12 +0200

    fix: remove unnecessary shadow from SolohostLogo component

--- components/SolohostLogo.vue
+++ components/SolohostLogo.vue
@@ -5,7 +5,7 @@
   >
     <div class="flex gap-3">
       <div
-        class="w-10 h-10 shrink-0 text-white font-bold text-2xl rounded-[13px] bg-[radial-gradient(100%_100%_at_30%_20%,#58d0ff_0%,#0ea5e9_40%,#04669e_100%)] grid place-items-center shadow-[0_10px_18px_rgba(14,165,233,.35)]"
+        class="w-10 h-10 shrink-0 text-white font-bold text-2xl rounded-[13px] bg-[radial-gradient(100%_100%_at_30%_20%,#58d0ff_0%,#0ea5e9_40%,#04669e_100%)] grid place-items-center"
         aria-hidden="true"
       >
         S
