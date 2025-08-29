7184d0a203ceaeab8bdf8c53e50657cb66074d8d
mktcode <kontakt@markus-kottlaender.de>
2025-08-29 17:27:09 +0200

    new welcome modal

--- components/profile/Admin/Welcome.vue
+++ components/profile/Admin/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useLocalStorage } from '@vueuse/core'
+import { useClipboard, useLocalStorage } from '@vueuse/core'
 
 const showModalLocal = useLocalStorage<boolean>('welcome', true)
 const showModal = ref(false)
@@ -12,6 +12,8 @@ function close() {
   showModal.value = false
   showModalLocal.value = false
 }
+
+const { copy, copied } = useClipboard()
 </script>
 
 <template>
@@ -24,17 +26,35 @@ function close() {
     <template #body>
       <div class="flex flex-col gap-4">
         <p class="text-gray-600">
-          Aktuell ist Ihre Website nur für Sie sichtbar. Oben links finden Sie die
-          <UIcon
-            name="i-heroicons-bars-3"
-            class="text-primary-500"
-          /> Administration, über die Sie Ihre Website verwalten können.
+          Ihre Website ist unter folgender Adresse erreichbar:
         </p>
+        <div class="text-gray-600 text-lg border border-gray-200 rounded-lg p-2 pl-3 flex items-center justify-between">
+          <span>
+            {{ $profile.canonicalUrl.replace(/https?:\/\//, '').replace(/\/$/, '') }}
+          </span>
+          <UButton
+            class="ml-2"
+            :label="copied ? 'kopiert!' : 'kopieren'"
+            variant="soft"
+            size="sm"
+            @click="copy($profile.canonicalUrl)"
+          />
+        </div>
         <p class="text-gray-600">
-          Für Fragen oder Feedback schreiben Sie uns gerne eine E-Mail an <a
-            href="mailto:support@beispiel.de"
-            class="italic"
-          >support@solohost.de</a>.
+          Aktuell ist sie <strong>nur für Sie sichtbar</strong>. Vervollständigen Sie in den <UIcon
+            name="i-heroicons-cog-6-tooth"
+            class="size-5 inline-block align-text-bottom"
+          /> Einstellungen Ihre <UIcon
+            name="i-heroicons-building-office-2"
+            class="size-5 inline-block align-text-bottom"
+          /> Unternehmensdaten, um Ihre Website veröffentlichen zu können.
+        </p>
+
+        <p>
+          Wenn Sie Unterstützung benötigen, egal ob mit Ihrer Website oder anderen Themen, erreichen Sie uns unter <UIcon
+            name="i-lucide-message-circle-question-mark"
+            class="size-5 inline-block align-text-bottom"
+          /> Hilfe.
         </p>
         <UButton
           label="Schließen"
