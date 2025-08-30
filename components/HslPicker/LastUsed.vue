7a4ae94e5ea456162b6cb54fd49a6fa5852cbf0d
mktcode <kontakt@markus-kottlaender.de>
2025-08-30 19:53:11 +0200

    reverse last used color list

--- components/HslPicker/LastUsed.vue
+++ components/HslPicker/LastUsed.vue
@@ -10,14 +10,12 @@ defineEmits<{
 
 <template>
   <div class="grid grid-cols-5 gap-1.5 border-t border-gray-200 p-2 pt-2 mt-2">
-    <TransitionGroup name="list">
-      <div
-        v-for="color in lastUsedColors"
-        :key="toHslString(color)"
-        :style="{ backgroundColor: toHslString(color) }"
-        class="w-full aspect-square rounded-lg cursor-pointer transition-all ring-2 ring-gray-100 hover:ring-gray-300"
-        @click="$emit('colorSelected', color)"
-      />
-    </TransitionGroup>
+    <div
+      v-for="color in lastUsedColors.slice().reverse()"
+      :key="toHslString(color)"
+      :style="{ backgroundColor: toHslString(color) }"
+      class="w-full aspect-square rounded-lg cursor-pointer transition-shadow ring-2 ring-gray-100 hover:ring-gray-300"
+      @click="$emit('colorSelected', color)"
+    />
   </div>
 </template>
