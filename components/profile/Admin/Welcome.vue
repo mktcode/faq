ca025bcacc6d8c5252e8e322846aafc05c712642
mktcode <kontakt@markus-kottlaender.de>
2025-08-30 12:28:34 +0200

    fix: improve wording and clarity in welcome modal text

--- components/profile/Admin/Welcome.vue
+++ components/profile/Admin/Welcome.vue
@@ -26,7 +26,7 @@ const { copy, copied } = useClipboard()
     <template #body>
       <div class="flex flex-col gap-4">
         <p class="text-gray-600">
-          Ihre Website ist unter folgender Adresse erreichbar:
+          Ihre Website ist unter der folgenden vorläufigen Adresse erreichbar:
         </p>
         <div class="text-gray-600 text-lg border border-gray-200 rounded-lg p-2 pl-3 flex items-center justify-between">
           <span>
@@ -41,17 +41,23 @@ const { copy, copied } = useClipboard()
           />
         </div>
         <p class="text-gray-600">
-          Aktuell ist sie <strong>nur für Sie sichtbar</strong>. Vervollständigen Sie in den <UIcon
+          Eine eigene Domain (z.B. meinefirma.de) können Sie in den <UIcon
+            name="i-heroicons-cog-6-tooth"
+            class="size-5 inline-block align-text-bottom"
+          /> Einstellungen verbinden.
+        </p>
+        <p class="text-gray-600">
+          Aktuell ist Ihre Website <strong>nur für Sie sichtbar</strong>. Vervollständigen Sie in den <UIcon
             name="i-heroicons-cog-6-tooth"
             class="size-5 inline-block align-text-bottom"
           /> Einstellungen Ihre <UIcon
             name="i-heroicons-building-office-2"
             class="size-5 inline-block align-text-bottom"
-          /> Unternehmensdaten, um Ihre Website veröffentlichen zu können.
+          /> Unternehmensdaten, um sie veröffentlichen zu können.
         </p>
 
         <p>
-          Wenn Sie Unterstützung benötigen, egal ob mit Ihrer Website oder anderen Themen, erreichen Sie uns unter <UIcon
+          Wenn Sie Unterstützung benötigen, egal ob mit Ihrer Website oder am Computer allgemein, erreichen Sie uns unter <UIcon
             name="i-lucide-message-circle-question-mark"
             class="size-5 inline-block align-text-bottom"
           /> Hilfe.
