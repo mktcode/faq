7b277cf917d4bee4a3b3f105877ed571dbafc7a5
mktcode <kontakt@markus-kottlaender.de>
2025-08-31 20:54:26 +0200

    refactor: enhance Thanks page layout and add animation for subscription completion

--- components/pages/Thanks.vue
+++ components/pages/Thanks.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
 appConfig.ui.colors.primary = 'sky'
+
+const animatedGreen = ref(false)
+setTimeout(() => {
+  animatedGreen.value = true
+}, 300)
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen text-2xl">
-    <SolohostLogo class="max-w-3xs mb-12" />
-    <p>
-      Vielen Dank für das Abschließen eines Abonnements. Sie können diese Seite nun schließen und zu Ihrer Website zurückkehren.
-    </p>
+  <div class="flex flex-col items-center h-screen text-2xl py-12">
+    <SolohostLogo class="max-w-3xs mb-24" />
+    <div class="flex flex-col items-center">
+      <UIcon
+        name="i-lucide-circle-check-big"
+        class="size-20 transition-colors duration-1000"
+        :class="{
+          'text-gray-200': !animatedGreen,
+          'text-green-600': animatedGreen
+        }"
+      />
+      <p class="max-w-xl mx-auto p-6 text-center">
+        Vielen Dank für das Abschließen eines Abonnements. Sie können diese Seite nun schließen und zu Ihrer Website zurückkehren.
+      </p>
+    </div>
   </div>
 </template>
