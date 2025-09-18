a16697be0d7bd6677732c15a92abbf661d69f0a4
mktcode <kontakt@markus-kottlaender.de>
2025-09-18 20:05:07 +0200

    feat: enhance drawer component styling for header and footer

--- app/components/profile/Admin/Website/Component.vue
+++ app/components/profile/Admin/Website/Component.vue
@@ -50,9 +50,9 @@ watch(component, () => {
       content: 'ring-0',
       container: 'relative no-scrollbar rounded-none p-0 gap-0 bg-white [box-shadow:0_0_18px_rgba(0,0,0,0.2)] max-w-lg',
       handle: '!w-24 !h-3 sm:!w-3 sm:!h-24 hover:!w-32 hover:sm:!h-32 hover:sm:!w-3 bottom-2 sm:bottom-0 sm:left-2 !bg-primary-500 !opacity-50 hover:!opacity-100 hover:!bg-primary-500 transition-all',
-      header: 'p-4 border-b border-gray-200',
+      header: 'p-4 border-b border-gray-200 sticky top-0 bg-white z-10',
       body: '',
-      footer: '!p-0',
+      footer: '!p-0 sticky bottom-0 bg-white z-10',
     }"
   >
     <template #header>
