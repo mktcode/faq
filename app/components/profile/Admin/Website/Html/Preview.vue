f3e886e45c8c580ba178ebb2824a899d61552db9
mktcode <kontakt@markus-kottlaender.de>
2025-09-19 15:41:30 +0200

    feat: update HTML component title and description for improved clarity

--- app/components/profile/Admin/Website/Html/Preview.vue
+++ app/components/profile/Admin/Website/Html/Preview.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="w-full">
-    html
+  <div class="flex flex-col gap-4 w-full max-w-xs">
+    <div
+      class="bg-gray-200 rounded-lg p-4 flex flex-col gap-4 items-center justify-center"
+    >
+      <UIcon name="i-heroicons-sparkles" class="size-12 text-gray-300" />
+
+      <div class="flex flex-col gap-2 w-1/2">
+        <div class="bg-gray-300 rounded-lg h-4 -ml-4" />
+        <div class="bg-gray-300 rounded-lg h-4 -mr-4" />
+      </div>
+    </div>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="bg-gray-200 rounded-lg h-8" />
+      <div class="bg-gray-100 rounded-lg h-8" />
+      <div class="bg-gray-300 rounded-lg h-8" />
+    </div>
   </div>
 </template>
