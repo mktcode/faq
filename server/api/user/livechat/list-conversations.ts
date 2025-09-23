f85e9a6171fbaafc7de5230441b8be06c235b8fd
mktcode <kontakt@markus-kottlaender.de>
2025-09-23 13:40:07 +0200

    fix: handle missing chatwoot source ID by creating a new contact

--- server/api/user/livechat/list-conversations.ts
+++ server/api/user/livechat/list-conversations.ts
@@ -1,11 +1,14 @@
 export default defineEventHandler(async (event) => {
   const me = await requireMe(event)
 
-  if (!me.chatwootSourceId) {
-    throw createError({ statusCode: 400, statusMessage: 'No chatwoot source ID set' })
+  let contactSourceId = me.chatwootSourceId
+
+  if (!contactSourceId) {
+    const newContact = await chatwoot.createContact(me.id, me.userName)
+    contactSourceId = newContact.source_id
   }
 
-  const conversations = await chatwoot.listConversations(me.chatwootSourceId)
+  const conversations = await chatwoot.listConversations(contactSourceId)
 
   const hasUnreadMessages = conversations.some(conv => conv.contact_last_seen_at < Math.max(...conv.messages.map(msg => msg.created_at)))
 
