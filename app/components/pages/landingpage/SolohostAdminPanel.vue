b55e0a13ee29e23f7ea3b7af62b0094ca8be1b9a
mktcode <kontakt@markus-kottlaender.de>
2025-09-25 21:20:40 +0200

    feat: add user table with expandable rows to admin panel

--- app/components/pages/landingpage/SolohostAdminPanel.vue
+++ app/components/pages/landingpage/SolohostAdminPanel.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui'
+import type { User } from '~~/types/db'
+import { h, resolveComponent } from 'vue'
+
+const UButton = resolveComponent('UButton')
+
 const toast = useToast()
 const { data: users } = await useFetch('/api/admin/users/all')
 const { data: nextSupportBookings, refresh: refreshNextSupportBookings } = await useFetch('/api/admin/supportBookings/next')
@@ -40,16 +46,61 @@ async function saveSupportDateTime() {
     isSavingSupportBooking.value = false
   }
 }
+
+const userExpanded = ref({ 1: false })
+const userColumns: TableColumn<User>[] = [
+  {
+    id: 'expand',
+    cell: ({ row }) =>
+      h(UButton, {
+        color: 'neutral',
+        variant: 'ghost',
+        icon: 'i-lucide-chevron-down',
+        square: true,
+        'aria-label': 'Expand',
+        ui: {
+          leadingIcon: [
+            'transition-transform',
+            row.getIsExpanded() ? 'duration-200 rotate-180' : ''
+          ]
+        },
+        onClick: () => row.toggleExpanded()
+      })
+  },
+  {
+    accessorKey: 'id',
+    header: '#',
+  },
+  {
+    accessorKey: 'userName',
+    header: 'Benutzername',
+  },
+  {
+    accessorKey: 'email',
+    header: 'E-Mail',
+  },
+  {
+    accessorKey: 'plan',
+    header: 'Paket',
+    cell: ({ row }) => row.getValue('plan') || 'Kein Paket'
+  },
+  {
+    accessorKey: 'lastPaidAt',
+    header: 'Letzte Zahlung',
+    cell: ({ row }) => new Date(row.getValue('lastPaidAt')).toLocaleDateString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' })
+  }
+]
 </script>
 
 <template>
   <div class="p-4">
-    <div class="w-[92vw] max-w-[1200px] mx-auto">
+    <div class="w-[92vw] max-w-[1200px] mx-auto flex items-center gap-2">
       <UPopover :ui="{ content: 'p-2 flex flex-col gap-2' }">
         <UButton
           icon="i-heroicons-calendar"
           label="Termin eintragen"
           color="neutral"
+          variant="soft"
         />
 
         <template #content>
@@ -88,6 +139,32 @@ async function saveSupportDateTime() {
             </div>
             <p v-if="!nextSupportBookings || nextSupportBookings.length === 0">Keine nächsten Termine.</p>
           </div>
+        </template>
+      </UPopover>
+
+      <UPopover>
+        <UButton
+          icon="i-heroicons-users"
+          label="Benutzer"
+          color="neutral"
+          variant="soft"
+        />
+
+        <template #content>
+          <UTable
+            v-if="users"
+            v-model:expanded="userExpanded"
+            :data="users"
+            :columns="userColumns"
+            class="flex-1"
+            :ui="{
+              tr: 'data-[expanded=true]:bg-elevated/50',
+            }"
+          >
+          <template #expanded="{ row }">
+            <pre class="max-h-[50vh] overflow-y-auto">{{ JSON.stringify(JSON.parse(row.original.settings), null, 2) }}</pre>
+          </template>
+          </UTable>
         </template>
       </UPopover>
     </div>
