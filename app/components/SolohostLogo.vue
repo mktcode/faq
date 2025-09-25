86681d3b044b0531128bdd4ebcc8eb7b946d8e01
mktcode <kontakt@markus-kottlaender.de>
2025-09-25 21:51:52 +0200

    fix: solohost logo layout

--- app/components/SolohostLogo.vue
+++ app/components/SolohostLogo.vue
@@ -7,7 +7,7 @@
       <img src="~/assets/img/logo.png" alt="Solohost Logo" class="w-10 shrink-0 rounded-[13px]"/>
       <div class="leading-5">
         <strong class="text-slate-900 dark:text-white text-lg leading-0">Solohost</strong><br>
-        <span class="text-gray-500 text-sm sm:text-base dark:text-gray-400">Webhosting<br class="sm:hidden" />&amp; IT-Support</span>
+        <span class="text-gray-500 text-sm sm:text-base dark:text-gray-400">Webhosting <br class="sm:hidden" />&amp; IT-Support</span>
       </div>
     </div>
   </a>
