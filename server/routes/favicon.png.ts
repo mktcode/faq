3a56a2115b16de1bd5d190590463309044766586
mktcode <kontakt@markus-kottlaender.de>
2025-10-01 20:45:15 +0200

    fix: favicon font

--- server/routes/favicon.png.ts
+++ server/routes/favicon.png.ts
@@ -33,13 +33,14 @@ async function getDefaultBuffer(companyName: string, color: { h: number; s: numb
       @font-face {
         font-family: 'FaviconRoboto';
         src: url(data:font/woff2;base64,${fontBase64}) format('woff2');
-        font-weight: 700;
         font-style: normal;
+        font-weight: 100 900;
+        font-stretch: 100%;
         font-display: swap;
+        unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
       }
       text {
         font-family: 'FaviconRoboto';
-        font-weight:700;
       }
     </style>`
     : ""
