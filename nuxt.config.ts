d80cb56ebf704800f1af706a5a498cad610a5b9e
mktcode <kontakt@markus-kottlaender.de>
2025-10-18 16:16:44 +0200

    feat: implement S3 and filesystem storage for user files in Nitro plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,24 +78,6 @@ export default defineNuxtConfig({
       },
     },
   },
-  nitro: {
-    storage: {
-      userfiles: {
-        driver: 's3',
-        accessKeyId: process.env.S3_ACCESS_KEY || process.env.NUXT_S3_ACCESS_KEY,
-        secretAccessKey: process.env.S3_SECRET_KEY || process.env.NUXT_S3_SECRET_KEY,
-        endpoint: process.env.S3_ENDPOINT || process.env.NUXT_PUBLIC_S3_ENDPOINT,
-        region: 'eu-central',
-        bucket: process.env.S3_BUCKET_NAME || process.env.NUXT_S3_BUCKET_NAME,
-      }
-    },
-    devStorage: {
-      userfiles: {
-        driver: 'fs',
-        base: './public/.userfiles'
-      }
-    }
-  },
   devServer: {
     host: 'solohost.local',
     https: {
---
+++ server/plugins/userfiles.ts
@@ -0,0 +1,20 @@
+import s3Driver from 'unstorage/drivers/s3'
+import fsDriver from 'unstorage/drivers/fs'
+
+export default defineNitroPlugin(() => {
+  const storage = useStorage()
+
+  const fs = fsDriver({
+    base: './public/.userfiles',
+  })
+
+  const s3 = s3Driver({
+    accessKeyId: useRuntimeConfig().s3AccessKey,
+    secretAccessKey: useRuntimeConfig().s3SecretKey,
+    endpoint: useRuntimeConfig().public.s3Endpoint,
+    region: useRuntimeConfig().s3Region,
+    bucket: useRuntimeConfig().s3Bucket,
+  })
+
+  storage.mount('userfiles', import.meta.dev ? fs : s3)
+})
